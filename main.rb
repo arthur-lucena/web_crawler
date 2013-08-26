ca4fd604ab1d905c906a8cb3141281448173da30
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-26 13:42:02 -0300

    Add Code

--- main.rb
+++ main.rb
@@ -1,1 +1,24 @@
-lol
+#!/usr/bin/env ruby
+ 
+require "rubygems"
+require "open-uri"
+require "nokogiri"
+require "yaml" 
+ 
+ 
+#puts("type URL: ")
+#url=gets
+ 
+#site = open("#{url}")
+site = open("http://g1.globo.com")
+ 
+doc = Nokogiri::HTML(site)
+ 
+#puts("type filter:")
+#filter=gets
+ 
+#elems = document.css("#{filter}").text
+elems = doc.css("a").each do |link|
+  puts link.content.gsub(/\s+/, "")
+end
+#puts YAML::dump(elems)
