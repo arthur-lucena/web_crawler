aba45a0a0a1cea6cab9c62fa1737646fffbd38be
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-26 17:15:31 -0300

    Update smain.rb

--- smain.rb
+++ smain.rb
@@ -2,8 +2,7 @@ require "rubygems"
 require "open-uri"
 require "nokogiri"
 
-site = open("http://www.softbox.com.br")
-doc = Nokogiri::HTML(site)
-elems = doc.css("a").each do |link|
-  puts  "url: " + link.attr("href").to_s + "\n"
+doc = Nokogiri::HTML(open("http://arthurvieira.com"))
+doc.css("a").each do |link|
+  puts  "href: " + link.attr("href") + "\n"
 end
