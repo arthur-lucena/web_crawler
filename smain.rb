0aed6bf9498755c76940d7c22d2a635fe9037e00
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2013-08-26 13:41:07 -0300

    Add code

--- smain.rb
+++ smain.rb
@@ -1,1 +1,9 @@
-gfdgdsfg
+require "rubygems"
+require "open-uri"
+require "nokogiri"
+
+site = open("http://www.softbox.com.br")
+doc = Nokogiri::HTML(site)
+elems = doc.css("a").each do |link|
+  puts  "url: " + link.attr("href").to_s + "\n"
+end
