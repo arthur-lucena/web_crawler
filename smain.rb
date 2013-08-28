99c511d55c89bdfd9d359cab64987e525ff0da93
Arthur Vieira Moreira Lucena <arthurvmx5@gmail,com>
2013-08-28 00:38:19 -0300

    regex url

--- smain.rb
+++ smain.rb
@@ -4,5 +4,10 @@ require "nokogiri"
 
 doc = Nokogiri::HTML(open("http://arthurvieira.com"))
 doc.css("a").each do |link|
-  puts  "href: " + link.attr("href") + "\n"
+  urlFull = link.attr("href")
+  urls = urlFull.scan(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/)#.split(/\s+/).find_all { |u| u =~ /https?:\/\/[\S]+/ }
+  
+  urls.each do |url|
+    puts url
+  end
 end
