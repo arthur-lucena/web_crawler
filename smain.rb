03ac813bf9a18154c08975feadbaba9194fca849
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2014-07-29 15:41:31 -0300

    Update smain.rb

--- smain.rb
+++ smain.rb
@@ -1,13 +1,38 @@
 require "rubygems"
 require "open-uri"
 require "nokogiri"
+require "yaml"
 
-doc = Nokogiri::HTML(open("http://arthurvieira.com"))
+config = YAML.load_file("config.json");
+url = config["url"]
+
+def isEmpty(param)
+	if !param.nil?
+		return param[2]
+	else 
+		return "vazio"
+	end
+end
+
+doc = Nokogiri::HTML(open(url))
 doc.css("a").each do |link|
   urlFull = link.attr("href")
-  urls = urlFull.scan(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/)#.split(/\s+/).find_all { |u| u =~ /https?:\/\/[\S]+/ }
+  urlBreaked = urlFull.scan(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/)#.split(/\s+/).find_all { |u| u =~ /https?:\/\/[\S]+/ }
+   
+  puts "urlBreaked.inspect: " + urlBreaked.inspect
+  puts "urlBreaked.class.name: " + urlBreaked.class.name
   
-  urls.each do |url|
-    puts url
-  end
+  puts "urlBreaked[0].inspect: " + urlBreaked[0].inspect
+  puts "urlBreaked[0].class.name: " + urlBreaked[0].class.name
+   
+  #puts "https: " + urlBreaked[0]
+  #puts "http: " + urlBreaked[1]
+  #puts "host: " + isEmpty(urlBreaked[0])
+  #puts "path: " + urlBreaked[3]
+  #puts "segments of path array: " + urlBreaked[4]
+  #puts "file: " + urlBreaked[5]
+  #puts "query: " + urlBreaked[6]
+  #puts "hash: " + urlBreaked[7]
+
 end
+
