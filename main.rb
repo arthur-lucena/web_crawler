36548c03e65a54c79fdf040b8ca6a97eaf59dc01
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2014-08-20 18:17:40 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,8 @@ require "rubygems"
 require "open-uri"
 require "nokogiri"
 require "yaml" 
- 
+
+#=> #<Encoding:UTF-8>
 
 config = YAML.load_file("config.json");
 url = config["url"]
@@ -13,14 +14,24 @@ url = config["url"]
  
 #site = open("#{url}")
 site = open(url)
- 
+
 doc = Nokogiri::HTML(site)
- 
+
+#encoding = nil;
+#doc.css('meta[http-equiv="Content-Type"]').each do |key|
+#	stringContent = key.attribute("content").content
+#	equalIndex = stringContent.index('=')
+#	encoding = stringContent[equalIndex + 1, stringContent.length]
+#end 
+#puts encoding
+
 #puts("type filter:")
 #filter=gets
  
 #elems = document.css("#{filter}").text
 elems = doc.css("a").each do |link|
-  puts link.content.gsub(/\s+/, "")
+	puts "---------------------------"
+	#puts YAML::dump(link)
+	puts link.content.strip#.gsub(/\s+/, "")
 end
 #puts YAML::dump(elems)
