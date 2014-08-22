e3ca42f623df0b167921bd88f35ff549c6f8c2ef
Arthur Vieira Moreira Lucena <arthurvmx5@gmail.com>
2014-08-22 14:03:11 -0300

    Update xmain.rb

--- xmain.rb
+++ xmain.rb
@@ -9,21 +9,33 @@ def matchContentLink (regex, document)
 	document.css("a").each do |link|
 		if regex.match(link.content)
 			puts "---------------------------"
-			puts link.content.strip#.gsub(/\s+/, "")
-			puts link.attribute("href")
+			puts link.content.strip
+			urlHref = link.attribute("href")
+
+			unless /http:\/\//i.match(urlHref)
+				urlHref = "http:/" + urlHref
+			end
+
+			puts urlHref
+
 			puts "\n"
-		else 
+		end
 	end
+end
+
+def openUrl (url)
+	site = open(url)
+
+	document = Nokogiri::HTML(site)
+
+	matchContentLink(/google(.*)/i, document)
 end
 
 def main 
 	config = YAML.load_file("config.json");
 	url = config["url"]
-	site = open(url)
-
-	doc = Nokogiri::HTML(site)
-
-	matchContentLink(/google(.*)/i, doc)
+	
+	openUrl(url)
 end
 
 main
